96431988f393c1bb7d4474e929995a52a4e38638
50BraulioAlvarez <braulioalvarezurban@gmail.com>
2020-07-31 22:04:31 -0500

    getUser Test

--- 05_Clase_Cristian/services/mock/userServices.js
+++ 05_Clase_Cristian/services/mock/userServices.js
@@ -25,6 +25,29 @@ function create(body) {
     })
 }
 
+
+function getUsers() {
+    const users = [
+        {
+            "is_admin": false,
+            "is_active": true,
+            "_id": "5f20c8c48778541248307180",
+            "name": "Pepito",
+            "last_name": "Perez",
+            "email": "Pepito@test.com",
+            "password": "contrasena",
+            "phone_number": 123245675,
+            "createdAt": "2020-07-29T00:54:28.834Z",
+            "updatedAt": "2020-07-29T00:54:28.834Z",
+            "__v": 0
+        }
+    ]
+    return new Promise((resolve) => {
+        resolve(users);
+    })
+}
+
 Services.create = create;
+Services.getUsers = getUsers;
 
 module.exports = Services;
--- 05_Clase_Cristian/test/UserController.spec.js
+++ 05_Clase_Cristian/test/UserController.spec.js
@@ -26,3 +26,25 @@ describe("User", () => {
         });
     });
 });
+describe("User", () => {
+    describe("Get User", () => {
+        if("Get", async () => {
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn(),
+            };
+            const req = {
+                body: {
+                    name:"",
+                    last_name:"",
+                    email:"",
+                    password:"",
+                    phone_number:"",
+                },
+            };
+            await userController.getUser(req, res);
+            expect(res.status.mock.calls).toEqual([[201]]);
+            expect(res.send.mock.calls).Equal([[expect.objectContaining({"is_active": true})]]);
+        });
+    });
+});
